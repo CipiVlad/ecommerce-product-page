f529eec4903e209414d7933ed6c4720595d66696
CipiVlad <supercoder.cipi@gmail.com>
2024-04-05 22:49:30 +0200

    setting up new workflow actions and pages

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const tsDummy: string = 'hello world'
+
+  return (
+
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>O Boy, {tsDummy}</h2>
+    </div>
+  )
+}
+
+export default App
