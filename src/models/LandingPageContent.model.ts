79f97dc1b69832436b803de37e9fac8b3b17f341
CipiVlad <supercoder.cipi@gmail.com>
2024-04-06 03:29:00 +0200

    introduced ImageGrid Component

--- src/models/LandingPageContent.model.ts
+++ src/models/LandingPageContent.model.ts
@@ -5,5 +5,7 @@ interface PageContent {
 }
 
 export const LandingPageContent: PageContent = {
-
+    title: "Fall Limited Edition Sneakers",
+    description: "These low-profile sneakers are your perfect casual wear companion. Featuring a classic look and modernised comfort, these shoes are perfect for any casual wear.",
+    image: "src/assets/images/image-product-1.jpg"
 }
