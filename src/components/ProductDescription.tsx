5fbb3cff2d8d089e8e56500f2d2dff701e5a5f2f
CipiVlad <supercoder.cipi@gmail.com>
2024-04-07 15:33:55 +0200

    product description added toFixed(2)

--- src/components/ProductDescription.tsx
+++ src/components/ProductDescription.tsx
@@ -5,13 +5,14 @@ import Counter from "./Counter";
 
 const ProductDescription = () => {
     const { title, description, price, discountPercentage, formerPrice, brand } = product;
+
     return (
         <div className="product_description_container">
             <p>{brand.toUpperCase()}</p>
             <h1>{title}</h1>
             <p>{description}</p>
-            <h3>${price} <span>{discountPercentage}%</span> </h3>
-            <p className="former_price">${formerPrice}</p>
+            <h3>${price.toFixed(2)} <span>{discountPercentage}%</span> </h3>
+            <p className="former_price">${formerPrice.toFixed(2)}</p>
             <div className="add_to_cart_container">
                 <Counter />
                 <AddToCart />
--- src/models/ProductDescription.model.ts
+++ src/models/ProductDescription.model.ts
@@ -11,7 +11,7 @@ export const product: ProductDescriptionModel = {
     brand: "Sneakers Company",
     title: "Fall Limited Edition Sneakers",
     description: "These low-profile sneakers are your perfect casual wear companion. Featuring a classic look and modernised comfort, these shoes are perfect for any casual wear.",
-    price: 125,
+    price: 125.00,
     discountPercentage: 50,
-    formerPrice: 250
+    formerPrice: 250.00
 }
