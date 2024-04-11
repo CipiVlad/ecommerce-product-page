24f4a546b81e5b04abc9b5ac5dd2321ef243cc7e
CipiVlad <supercoder.cipi@gmail.com>
2024-04-11 15:12:04 +0200

    svg color fill on hover with states and onMouseEnter/Leave

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,12 +2,13 @@ import '../sass/ImageGrid.scss'
 import { bigImages, thumbnails } from '../models/Images.model'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useEffect, useState } from 'react'
-import close from '../assets/images/icon-close.svg'
-import next from '../assets/images/icon-next.svg'
-import prev from '../assets/images/icon-previous.svg'
+
 
 const Modal = ({ openModal, setOpenModal }: { openModal: boolean, setOpenModal: React.Dispatch<React.SetStateAction<boolean>> }) => {
     const [activeThumbIndex, setActiveThumbIndex] = useState<number | null>(null);
+    const [fill_X, setFill_X] = useState("");
+    const [fill_right_arrow, setFill_right_arrwo] = useState("");
+    const [fill_left_arrow, setFill_left_arrow] = useState("");
 
     const handleNextClick = () => {
         if (activeThumbIndex !== null && activeThumbIndex < thumbnails.length - 1) {
@@ -61,20 +62,44 @@ const Modal = ({ openModal, setOpenModal }: { openModal: boolean, setOpenModal: 
                         display: "grid"
                     }}>
 
-                        <div className="close_modal hi" onClick={closeModal}>
-                            <img src={close} alt="close" width={20} style={{ cursor: "pointer" }} />
+                        <div className="close_modal" onClick={closeModal}>
+                            <svg width="30" height="20" xmlns="http://www.w3.org/2000/svg"
+                                onMouseEnter={() => setFill_X("#fff")}
+                                onMouseLeave={() => setFill_X("")}
+                            ><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                                fill={fill_X ? "hsl(26, 100%, 55%)" : "#fff"} fillRule="evenodd"
+                                style={{ cursor: "pointer" }}
+
+                                /></svg>
                         </div>
                         <div className="bigImage">
-                            <img src={prev} alt="prev" onClick={handlePrevClick} className='arrow_prev'
-                                style={{ width: "18px", height: "18px", padding: "20px", position: "absolute", left: "-20px", top: "40%" }} />
+                            <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg"
+                                className="arrow_prev"
+                                style={{ width: "18px", height: "18px", padding: "20px", position: "absolute", left: "-20px", top: "40%" }}
+                                onClick={handlePrevClick}
+                                onMouseEnter={() => setFill_left_arrow("#1D2026")}
+                                onMouseLeave={() => setFill_left_arrow("")}
+                            >
+                                <path d="M11 1 3 9l8 8" stroke={fill_left_arrow ? "hsl(26, 100%, 55%)" : "#1D2026"} stroke-width="2" fill="none" fillRule="evenodd"
+
+                                />
+                            </svg>
+
                             {activeThumbIndex !== null && (
                                 <img
                                     src={bigImages[activeThumbIndex].src}
                                     alt={bigImages[activeThumbIndex].alt}
                                 />
                             )}
-                            <img src={next} alt="next" onClick={handleNextClick} className='arrow_next'
-                                style={{ width: "18px", height: "18px", padding: "20px", position: "absolute", right: "-20px", top: "40%" }} />
+                            <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg"
+                                className="arrow_next"
+                                style={{ width: "18px", height: "18px", padding: "20px", position: "absolute", right: "-20px", top: "40%" }}
+                                onClick={handleNextClick}
+                                onMouseEnter={() => setFill_right_arrwo("#1D2026")}
+                                onMouseLeave={() => setFill_right_arrwo("")}
+                            ><path d="m2 1 8 8-8 8" stroke={fill_right_arrow ? "hsl(26, 100%, 55%)" : "#1D2026"} stroke-width="2" fill="none" fillRule="evenodd"
+
+                                /></svg>
                         </div>
                         <div className="small_images_modal">
                             <div>
