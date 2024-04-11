7a10af52e13c9a11c7bbe850f450ad8cd707faae
CipiVlad <supercoder.cipi@gmail.com>
2024-04-11 14:08:50 +0200

    bug fix on the fly: z-index: auto to menuBox when modal opens

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,13 +1,25 @@
-
 import '../sass/ImageGrid.scss'
-
 import { bigImages, thumbnails } from '../models/Images.model'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import close from '../assets/images/icon-close.svg'
+import next from '../assets/images/icon-next.svg'
+import prev from '../assets/images/icon-previous.svg'
 
 const Modal = ({ openModal, setOpenModal }: { openModal: boolean, setOpenModal: React.Dispatch<React.SetStateAction<boolean>> }) => {
     const [activeThumbIndex, setActiveThumbIndex] = useState<number | null>(null);
+
+    const handleNextClick = () => {
+        if (activeThumbIndex !== null && activeThumbIndex < thumbnails.length - 1) {
+            setActiveThumbIndex(activeThumbIndex + 1);
+        }
+    };
+
+    const handlePrevClick = () => {
+        if (activeThumbIndex !== null && activeThumbIndex > 0) {
+            setActiveThumbIndex(activeThumbIndex - 1);
+        }
+    };
 
     const handleThumbnailClick = (index: number) => {
         setActiveThumbIndex(index);
@@ -41,22 +53,28 @@ const Modal = ({ openModal, setOpenModal }: { openModal: boolean, setOpenModal: 
                         display: "grid"
                     }}
                 >
+
                     <div className="image_grid modal" style={{
                         position: "absolute",
                         top: "15vh",
                         left: "35vw",
                         display: "grid"
                     }}>
-                        <div className="close_modal" onClick={closeModal}>
+
+                        <div className="close_modal hi" onClick={closeModal}>
                             <img src={close} alt="close" width={20} style={{ cursor: "pointer" }} />
                         </div>
                         <div className="bigImage">
+                            <img src={prev} alt="prev" onClick={handlePrevClick} className='arrow_prev'
+                                style={{ width: "18px", height: "18px", padding: "20px", position: "absolute", left: "-20px", top: "40%" }} />
                             {activeThumbIndex !== null && (
                                 <img
                                     src={bigImages[activeThumbIndex].src}
                                     alt={bigImages[activeThumbIndex].alt}
                                 />
                             )}
+                            <img src={next} alt="next" onClick={handleNextClick} className='arrow_next'
+                                style={{ width: "18px", height: "18px", padding: "20px", position: "absolute", right: "-20px", top: "40%" }} />
                         </div>
                         <div className="small_images_modal">
                             <div>
@@ -71,9 +89,11 @@ const Modal = ({ openModal, setOpenModal }: { openModal: boolean, setOpenModal: 
                                 ))}
                             </div>
                         </div>
+
                     </div>
                 </motion.div>
             )}
+
         </AnimatePresence>
     );
 };
